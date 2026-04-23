e72f300e254384557abf8c44c3b87e2cf8f185bf
A.B. Siddik <57783672+absiddik517@users.noreply.github.com>
2026-04-23 15:34:06 +0600

    Update Folder.php

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -4,9 +4,11 @@ namespace App\Models;
 use App\Models\Office;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Folder extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'office_id',
         'name',
